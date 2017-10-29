1814ca5f617be3b7d9610af3efd44fc176056871
huzht5 <32868765+huzht5@users.noreply.github.com>
2017-10-29 20:59:52 +0800

    Add files via upload

---
+++ Agenda/ReadMe.txt
@@ -0,0 +1,10 @@
+在控制终端里进入到agenda/目录下，即可输入命令
+用户注册 go run main.go register -u=[username] -p=[password] -e=[email] -n=[phoneNumber]
+用户登录 go run main.go login -u=[username] -p=[password]
+用户登出 go run main.go logout
+用户查询 go run main.go query
+用户删除 go run main.go delete
+上述命令里的[]中的参数是用户输入的参数 username用户名 password用户密码 email邮箱地址 phoneNumber手机号码
+
+小组成员：胡志通 15331110
+          胡泽华 15331109
---
+++ Agenda/agenda/cmd/login.go
@@ -0,0 +1,49 @@
+// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+  "agenda/user"
+
+	"github.com/spf13/cobra"
+)
+
+// loginCmd represents the login command
+var loginCmd = &cobra.Command{
+	Use:   "login",
+	Short: "login",
+	Long: `You can login your account`,
+	Run: func(cmd *cobra.Command, args []string) {
+		username, _ := cmd.Flags().GetString("UserName")
+		password, _ := cmd.Flags().GetString("PassWord")
+		user.Login(&username, &password)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(loginCmd)
+
+	loginCmd.Flags().StringP("UserName", "u", "Anonymous", "Your UserName")
+	loginCmd.Flags().StringP("PassWord", "p", "123456", "Your PassWord")
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// loginCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// loginCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
